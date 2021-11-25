59b3ca3f6ec680cfb99809e31bbf4815100ae23a
tridi <test31122001@gmail.com>
2021-11-25 21:51:03 +0700

    mod: sql, dummy data

--- sql/Dummy Data.sql
+++ sql/Dummy Data.sql
@@ -11,93 +11,74 @@ delete from customer_account;
 delete from order_product;
 delete from ordered_product;
 
-insert into supplier (nama_supplier, alamat, nomor_hp, email, kode_pos) 
-values ('Ahmad', 'Jalan Merdeka', '0871254562', 'ahmad@gmail.com', '9001');
-insert into supplier (nama_supplier, alamat, nomor_hp, email, kode_pos) 
-values ('Fadel', 'Jalan Tidak Merdeka', '08123645771', 'fadel@gmail.com', '9002');
-insert into supplier (nama_supplier, alamat, nomor_hp, email, kode_pos) 
-values ('Abyan', 'Jalan Kapan Merdeka', '0825184551', 'ahmad@gmail.com', '9003');
-insert into supplier (nama_supplier, alamat, nomor_hp, email, kode_pos) 
-values ('Fauzan', 'Jalan Sudah Mvalues erdeka', '0813254882', 'fauzan@gmail.com', '9004');
-insert into supplier (nama_supplier, alamat, nomor_hp, email, kode_pos) 
-values ('Hassan', 'Jalan Belum Merdeka', '036744125', 'hassan@gmail.com', '9005');
+-- TABEL SUPPLIER
+insert into supplier (nama_supplier, alamat, nomor_hp, email, kode_pos) values ('Ahmad', 'Jalan Merdeka', '0871254562', 'ahmad@gmail.com', '9001');
+insert into supplier (nama_supplier, alamat, nomor_hp, email, kode_pos) values ('Fadel', 'Jalan Tidak Merdeka', '08123645771', 'fadel@gmail.com', '9002');
+insert into supplier (nama_supplier, alamat, nomor_hp, email, kode_pos) values ('Abyan', 'Jalan Kapan Merdeka', '0825184551', 'ahmad@gmail.com', '9003');
+insert into supplier (nama_supplier, alamat, nomor_hp, email, kode_pos) values ('Fauzan', 'Jalan Sudah Mvalues erdeka', '0813254882', 'fauzan@gmail.com', '9004');
+insert into supplier (nama_supplier, alamat, nomor_hp, email, kode_pos) values ('Hassan', 'Jalan Belum Merdeka', '036744125', 'hassan@gmail.com', '9005');
 
-insert into kategori values ('buah', 'kategori yang berisi buah-buahan');
-insert into kategori values ('sayur', 'kategori yang berisi sayur-sayuran');
+-- TABEL KATEGORI
+insert into kategori values ('Buah', 'kategori yang berisi buah-buahan');
+insert into kategori values ('Sayur', 'kategori yang berisi sayur-sayuran');
 
-insert into produk (nama_produk, jumlah_stok, harga_satuan, deskripsi, nutrition_facts, kategori) 
-values ('Mangga', 40, 15000, 'mangga segar dan sehat', 'vitamin a', 'buah');
-insert into produk (nama_produk, jumlah_stok, harga_satuan, deskripsi, nutrition_facts, kategori) 
-values ('Pisang', 30, 10000, 'pisang segar dan sehat', 'vitamin a,b,c', 'buah');
-insert into produk (nama_produk, jumlah_stok, harga_satuan, deskripsi, nutrition_facts, kategori) 
-values ('Timun', 60, 7000, 'timun segar dan sehat', 'vitamin c,k', 'sayur');
-insert into produk (nama_produk, jumlah_stok, harga_satuan, deskripsi, nutrition_facts, kategori) 
-values ('Tomat', 50, 8000, 'tomat segar dan sehat', 'vitamin b,c', 'sayur');
-insert into produk (nama_produk, jumlah_stok, harga_satuan, deskripsi, nutrition_facts, kategori) 
-values ('Apel', 15, 30000, 'apel segar dan sehat', 'vitamin a,c,k', 'buah');
+-- TABEL PRODUK
+insert into produk (nama_produk, jumlah_stok, harga_satuan, deskripsi, nutrition_facts, kategori) values ('Mangga', 40, 15000, 'mangga segar dan sehat', 'vitamin a', 'buah');
+insert into produk (nama_produk, jumlah_stok, harga_satuan, deskripsi, nutrition_facts, kategori) values ('Pisang', 30, 10000, 'pisang segar dan sehat', 'vitamin a,b,c', 'buah');
+insert into produk (nama_produk, jumlah_stok, harga_satuan, deskripsi, nutrition_facts, kategori) values ('Timun', 60, 7000, 'timun segar dan sehat', 'vitamin c,k', 'sayur');
+insert into produk (nama_produk, jumlah_stok, harga_satuan, deskripsi, nutrition_facts, kategori) values ('Tomat', 50, 8000, 'tomat segar dan sehat', 'vitamin b,c', 'sayur');
+insert into produk (nama_produk, jumlah_stok, harga_satuan, deskripsi, nutrition_facts, kategori) values ('Apel', 15, 30000, 'apel segar dan sehat', 'vitamin a,c,k', 'buah');
 
+-- TABEL SUPPLIED_PRODUCT
 insert into supplied_product values (1, 1, 50);
 insert into supplied_product values (2, 2, 25);
 insert into supplied_product values (3, 3, 45);
 insert into supplied_product values (4, 4, 10);
 insert into supplied_product values (5, 5, 15);
 
+-- TABEL JABATAN
 insert into jabatan values ('Manager', 6000000);
 insert into jabatan values ('Marketing', 4500000);
 insert into jabatan values ('Kurir', 3000000);
 
+-- TABEL CABANG
 insert into cabang (nama_cabang, alamat_cabang) values ('cabang malang', 'jalan veteran');
 insert into cabang (nama_cabang, alamat_cabang) values ('cabang bali', 'jalan kuta');
 insert into cabang (nama_cabang, alamat_cabang) values ('cabang jakarta', 'jalan kuningan');
 
-insert into karyawan (nama, tanggal_lahir, nomor_hp, email, alamat, id_cabang, jabatan) 
-values ('Faisal', '1998-08-14', '081745562', 'faisal@gmail.com', 'Jalan Sesetan', 1, 'Kurir');
-insert into karyawan (nama, tanggal_lahir, nomor_hp, email, alamat, id_cabang, jabatan) 
-values ('Hakim', '1997-12-05', '082546587', 'hakim@gmail.com', 'Jalan Pedungan', 1, 'Manager');
-insert into karyawan (nama, tanggal_lahir, nomor_hp, email, alamat, id_cabang, jabatan) 
-values ('Ridwan', '1997-02-11', '08695123', 'ridwan@gmail.com', 'Jalan Hautang', 2, 'Manager');
-insert into karyawan (nama, tanggal_lahir, nomor_hp, email, alamat, id_cabang, jabatan) 
-values ('Mahameruh', '1998-06-13', '08265781', 'mahameruh@gmail.com', 'Jalan Veteran', 3, 'Manager');
-insert into karyawan (nama, tanggal_lahir, nomor_hp, email, alamat, id_cabang, jabatan) 
-values ('Nisai', '1999-11-04', '0875422156', 'nisai@gmail.com', 'Jalan Merdeka', 3, 'Marketing');
+-- TABEL KARYAWAN
+insert into karyawan (nama, jenis_kelamin, tanggal_lahir, nomor_hp, email, alamat, id_cabang, jabatan) values ('Faisal', 'L', '1998-08-14', '081745562', 'faisal@gmail.com', 'Jalan Sesetan', 1, 'Kurir');
+insert into karyawan (nama, jenis_kelamin, tanggal_lahir, nomor_hp, email, alamat, id_cabang, jabatan) values ('Hakim', 'L', '1997-12-05', '082546587', 'hakim@gmail.com', 'Jalan Pedungan', 1, 'Manager');
+insert into karyawan (nama, jenis_kelamin, tanggal_lahir, nomor_hp, email, alamat, id_cabang, jabatan) values ('Ridwan', 'L', '1997-02-11', '08695123', 'ridwan@gmail.com', 'Jalan Hautang', 2, 'Manager');
+insert into karyawan (nama, jenis_kelamin, tanggal_lahir, nomor_hp, email, alamat, id_cabang, jabatan) values ('Mahameruh', 'L', '1998-06-13', '08265781', 'mahameruh@gmail.com', 'Jalan Veteran', 3, 'Manager');
+insert into karyawan (nama, jenis_kelamin, tanggal_lahir, nomor_hp, email, alamat, id_cabang, jabatan) values ('Nisai', 'L', '1999-11-04', '0875422156', 'nisai@gmail.com', 'Jalan Merdeka', 3, 'Marketing');
 
-insert into customer_profile  (nama_pelanggan, tanggal_lahir, nomor_hp, nomor_rumah, desa_kecamatan, kabupaten_kota, jalan, jenis_kelamin, kode_pos) 
-values ('Victoria', '1913-04-02', '0526715754', 8, 'Batu', 'Malang', 'Gang Surapati', 'P', '81124');
-insert into customer_profile  (nama_pelanggan, tanggal_lahir, nomor_hp, nomor_rumah, desa_kecamatan, kabupaten_kota, jalan, jenis_kelamin, kode_pos) 
-values ('Paramita', '1920-12-15', '082608102', 99, 'Malang', 'Malang', 'Gang S. Parman', 'P', '51147');
-insert into customer_profile  (nama_pelanggan, tanggal_lahir, nomor_hp, nomor_rumah, desa_kecamatan, kabupaten_kota, jalan, jenis_kelamin, kode_pos) 
-values('Samsul', '1913-04-02', '0351367322', 2, 'Pedungan', 'Denpasar', 'Jl. Jamika', 'L', '80002');
-insert into customer_profile  (nama_pelanggan, tanggal_lahir, nomor_hp, nomor_rumah, desa_kecamatan, kabupaten_kota, jalan, jenis_kelamin, kode_pos) 
-values('Paiman', '1924-09-14', '0886705467', 8, 'Kuningan', 'Jakarta', 'Jl. Medokan Ayu', 'L', '25557');
+-- TABEL CUSTOMER ACCOUNT
+insert into customer_account values ('victoria@gmail.com', 'victoriapw');
+insert into customer_account values ('paramita@gmail.com', 'paramitapw');
+insert into customer_account values ('samsul@gmail.com', 'samsulpw');
+insert into customer_account values ('paiman@gmail.com', 'paimanpw');
 
-insert into customer_account values (1, 'victoria@gmail.com', 'victoriapw');
-insert into customer_account values (2 ,'paramita@gmail.com', 'paramitapw');
-insert into customer_account values (3, 'samsul@gmail.com', 'samsulpw');
-insert into customer_account values (4, 'paiman@gmail.com', 'paimanpw');
+-- TABEL CUSTOMER PROFILE
+insert into customer_profile (id_pelanggan, nama_pelanggan, tanggal_lahir, nomor_hp, nomor_rumah, desa_kecamatan, kabupaten_kota, jalan, jenis_kelamin, kode_pos) values (1 ,'Victoria', '1913-04-02', '0526715754', 8, 'Batu', 'Malang', 'Gang Surapati', 'P', '81124');
+insert into customer_profile (id_pelanggan, nama_pelanggan, tanggal_lahir, nomor_hp, nomor_rumah, desa_kecamatan, kabupaten_kota, jalan, jenis_kelamin, kode_pos) values (2 ,'Paramita', '1920-12-15', '082608102', 99, 'Malang', 'Malang', 'Gang S. Parman', 'P', '51147');
+insert into customer_profile (id_pelanggan, nama_pelanggan, tanggal_lahir, nomor_hp, nomor_rumah, desa_kecamatan, kabupaten_kota, jalan, jenis_kelamin, kode_pos) values (3 ,'Samsul', '1913-04-02', '0351367322', 2, 'Pedungan', 'Denpasar', 'Jl. Jamika', 'L', '80002');
+insert into customer_profile (id_pelanggan, nama_pelanggan, tanggal_lahir, nomor_hp, nomor_rumah, desa_kecamatan, kabupaten_kota, jalan, jenis_kelamin, kode_pos) values (4 ,'Paiman', '1924-09-14', '0886705467', 8, 'Kuningan', 'Jakarta', 'Jl. Medokan Ayu', 'L', '25557');
 
-insert into order_product (tanggal_kirim, status_order, id_pelanggan, id_karyawan) 
-values ('2021-05-11', 1, 1, 2)
-insert into order_product (tanggal_kirim, status_order, id_pelanggan, id_karyawan)
-values ('2021-06-12', 0, 2, 1)
-insert into order_product (tanggal_kirim, status_order, id_pelanggan, id_karyawan) 
-values ('2021-06-15', 0, 3, 3)
-insert into order_product (tanggal_kirim, status_order, id_pelanggan, id_karyawan) 
-values ('2021-06-15', 0, 3, 3)
-insert into order_product (tanggal_kirim, status_order, id_pelanggan, id_karyawan) 
-values ('2021-06-17', 0, 4, 4)
+-- TABEL ORDER_PRODUCT
+insert into order_product (tanggal_kirim, status_order, id_pelanggan, id_karyawan) values ('2021-05-11', 1, 1, 2)
+insert into order_product (tanggal_kirim, status_order, id_pelanggan, id_karyawan)values ('2021-06-12', 0, 2, 1)
+insert into order_product (tanggal_kirim, status_order, id_pelanggan, id_karyawan) values ('2021-06-15', 0, 3, 3)
+insert into order_product (tanggal_kirim, status_order, id_pelanggan, id_karyawan) values ('2021-06-15', 0, 3, 3)
+insert into order_product (tanggal_kirim, status_order, id_pelanggan, id_karyawan) values ('2021-06-17', 0, 4, 4)
 
-insert into ordered_product (order_id, product_id, kuantitas, harga_product)
-values (1, 1, 3, 50000)
-insert into ordered_product (order_id, product_id, kuantitas, harga_product)
-values (2, 2, 9, 50000)
-insert into ordered_product (order_id, product_id, kuantitas, harga_product)
-values (3, 3, 10, 50000)
-insert into ordered_product (order_id, product_id, kuantitas, harga_product)
-values (4, 4, 5, 50000)
-insert into ordered_product (order_id, product_id, kuantitas, harga_product)
-values (4, 1, 2, 10000)
-insert into ordered_product (order_id, product_id, kuantitas, harga_product)
-values (3, 2, 11, 5000)
+-- TABEL ORDERED_PRODUCT
+insert into ordered_product (order_id, product_id, kuantitas, harga_product)values (1, 1, 3, 50000)
+insert into ordered_product (order_id, product_id, kuantitas, harga_product)values (2, 2, 9, 50000)
+insert into ordered_product (order_id, product_id, kuantitas, harga_product)values (3, 3, 10, 50000)
+insert into ordered_product (order_id, product_id, kuantitas, harga_product)values (4, 4, 5, 50000)
+insert into ordered_product (order_id, product_id, kuantitas, harga_product)values (4, 1, 2, 10000)
+insert into ordered_product (order_id, product_id, kuantitas, harga_product)values (3, 2, 11, 5000)
 
 -- check semua tabel
 select * from cabang
--- sql/Query Project.sql
+++ sql/Query Project.sql
@@ -8,11 +8,10 @@ go
 create database Project
 go
 use Project
-
 go
 
 create table kategori (
-    nama_kategori varchar(30) primary key,
+    nama_kategori varchar(100) primary key,
     deskripsi text
 )
 
@@ -23,47 +22,47 @@ create table produk (
     harga_satuan int not null,
     deskripsi text,
     nutrition_facts text,
-    kategori varchar(30) foreign key references kategori(nama_kategori) on delete set null
+    kategori varchar(100) foreign key references kategori(nama_kategori) on delete set null
 )
 
 create table cabang(
     id_cabang int primary key identity(1,1),
-    nama_cabang varchar(40) not null,
+    nama_cabang varchar(100) not null,
     alamat_cabang text not null
 )
 
 create table jabatan(
-    nama_jabatan varchar(30) primary key,
+    nama_jabatan varchar(50) primary key,
     gaji int not null,
 )
 
 create table karyawan (
     id_karyawan int primary key identity(1,1),
-    nama varchar(50) not null,
+    nama varchar(255) not null,
     jenis_kelamin char not null,
     tanggal_lahir date not null,
     nomor_hp varchar(25) not null,
     email varchar(50) not null,
     alamat text not null,
     id_cabang int foreign key references cabang(id_cabang) on delete set null,
-    jabatan varchar(30) foreign key references jabatan(nama_jabatan) on delete set null
+    jabatan varchar(50) foreign key references jabatan(nama_jabatan) on delete set null
 )
 
 create table customer_account (
     id_pelanggan int primary key identity(1,1),
     email varchar(50) not null,
-    password varchar(50) not null,
+    password varchar(255) not null,
 )
 
 create table customer_profile (
     id_pelanggan int primary key,
-    nama_pelanggan varchar(50) not null,
+    nama_pelanggan varchar(255) not null,
     tanggal_lahir date not null,
     nomor_hp varchar(25) not null,
     nomor_rumah int not null,
-    desa_kecamatan varchar(20) not null,
-    kabupaten_kota varchar(20) not null,
-    jalan varchar(30) not null,
+    desa_kecamatan varchar(50) not null,
+    kabupaten_kota varchar(50) not null,
+    jalan varchar(50) not null,
     jenis_kelamin char not null,
     kode_pos varchar(5) not null
 
@@ -72,7 +71,7 @@ create table customer_profile (
 
 create table supplier (
     id_supplier int primary key identity(1,1),
-    nama_supplier varchar(50) not null,
+    nama_supplier varchar(255) not null,
     alamat text not null,
     nomor_hp varchar(20) not null,
     email varchar(50) not null,
@@ -88,7 +87,8 @@ create table supplied_product (
     foreign key (id_supplier) references supplier(id_supplier) on delete no action
 )
 
-create table order_product (
+-- Nama aslinya order : jadi order_product karena nama "order" tidak bisa digunakan
+create table order_product(
     order_id int primary key identity(1,1),
     tanggal_kirim date not null,
     status_order bit not null default 0,
